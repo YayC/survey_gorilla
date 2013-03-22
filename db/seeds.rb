d46cdde9d43930a0a0d41b70d5ddf5a91acf4a34
Apprentice <apprentice@devbootcamp.com>
2013-03-22 16:37:49 -0700

    fixed bugs in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 100.times do
-  user = User.new name: Faker::Name.name, email: Faker::Internet.email)
+  user = User.new name: Faker::Name.name, email: Faker::Internet.email
   user.password = Faker::Lorem.word
   user.save
 end
@@ -10,7 +10,7 @@ u = User.new(name: "test", email: "test@example.com"); u.password="test"; u.save
 
 
 100.times do |i|
-  surv = Survey.create(creator_id: rand(1..100), title: Faker::Lorem.sentence(rand(1..4)), description: Fake::Lorem.paragraph(rand(1..2)))
+  surv = Survey.create(creator_id: rand(1..100), title: Faker::Lorem.sentence(rand(1..4)), description: Faker::Lorem.paragraph(rand(1..2)))
   rand(5..15).times do
     question = Question.create(content: Faker::Lorem.sentence(rand(1..2)) + "?")
       4.times do
@@ -22,7 +22,7 @@ u = User.new(name: "test", email: "test@example.com"); u.password="test"; u.save
 end
 
 100.times do
-  cs = completed_surveys.create( user_id: rand(1..100), survey_id: rand(1..100))
+  cs = CompletedSurvey.create( user_id: rand(1..100), survey_id: rand(1..100))
   Survey.find(cs.survey_id).questions.each do |question|
     choice_id = question.choices.sample.id
     Answer.create( choice_id: choice_id, user_id: cs.user_id)
