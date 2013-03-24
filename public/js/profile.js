f582ae94272585b5b0e0fff06a9187c05ce9ae20
Apprentice <apprentice@devbootcamp.com>
2013-03-23 22:36:57 -0700

    ajax_redemption_aka_where_my_saturday_night_went

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,56 +1,28 @@
 $(document).ready(function() {
 
-  $('.delete').click(function(e) {
-    e.preventDefault();
-  
-    $.ajax({
-      type: 'get',                // Q: what is "this"?
-      url: this.href,
-      data: '',                       // NOTE: the var 'data' above
-    })
-    // 3a- this is called when the AJAX call
-    //     succeeds
-    .done(function(server_data, textStatus, jqXHR) {
-      console.log('Success' + server_data);
-      // 3b- update the DOM with the data returned
-      //     by the server
+    function reset_survey_listeners() {
+    $('.delete').click(function(e) {
+      e.preventDefault();
+    
+      $.ajax({
+        type: 'get',                
+        url: this.href,
+        data: '',                       
+      })
+   
+      .done(function(server_data, textStatus, jqXHR) {
+        console.log('Success' + server_data);
 
-      $("#my_surveys").html(server_data);
-    })
-    // 3c- this is called when the AJAX call
-    //     fails
-    .fail(function(jqXHR, textStatus, errorThrown) {
-      console.log("ERROR -- " + errorThrown);
+        $("#my_surveys").html(server_data);
+        reset_survey_listeners();
+      })
 
-    })
-  });
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("ERROR -- " + errorThrown);
 
+      })
+    });
+  };
 
-
-  // $('.delete').click(function(e) {
-  //   e.preventDefault();
-  
-  //   $.ajax({
-  //     type: 'get',                // Q: what is "this"?
-  //     url: this.href,
-  //     data: '',                       // NOTE: the var 'data' above
-  //   })
-  //   // 3a- this is called when the AJAX call
-  //   //     succeeds
-  //   .done(function(server_data, textStatus, jqXHR) {
-  //     console.log('Success' + server_data);
-  //     // 3b- update the DOM with the data returned
-  //     //     by the server
-
-  //     $("#my_surveys").html(server_data);
-  //   })
-  //   // 3c- this is called when the AJAX call
-  //   //     fails
-  //   .fail(function(jqXHR, textStatus, errorThrown) {
-  //     console.log("ERROR -- " + errorThrown);
-
-  //   })
-
-  // });
-
+  reset_survey_listeners();
 });
